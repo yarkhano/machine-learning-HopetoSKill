cccee5f51355605972c8a100eb789a382e52446e
unknown <muhammadyarkhan31@gmail.com>
2026-03-12 00:25:47 +0500

    will work on on titanic dataset for preprocessing

--- lecture_04.py
+++ lecture_04.py
@@ -1,4 +1,8 @@
 #Data Preprocessing, This file include code of lecture 04 as well as the code i have added by myself
+#Data cleaning ,outliers detection,finding missing values.
 
 from sklearn.datasets import load_iris
 import pandas as pd
+import seaborn as sns
+
+dataset = sns.load_dataset("titanic")
