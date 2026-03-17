9cb3c8ed576810f4c478cb8901443b468be9756c
unknown <muhammadyarkhan31@gmail.com>
2026-03-17 23:16:03 +0500

    looking to data

--- lecture_04.py
+++ lecture_04.py
@@ -1,4 +1,4 @@
-#Data Preprocessing part 1, This file include code of lecture 04 as well as the code i have added by myself
+#Data Preprocessing part 1, This file include code of lecture 04 as well as the code i have added by myself.
 #Data cleaning ,outliers detection,finding missing values.
 
 import pandas as pd
@@ -6,4 +6,8 @@ import seaborn as sns
 
 dataset = sns.load_dataset("titanic")
 print(dataset.shape)
+print(dataset.head(10))
+print(dataset.describe()) #this give statistical sunmmary of numerica columns(mean,min max)
+
+
 
