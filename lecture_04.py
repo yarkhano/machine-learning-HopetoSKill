62ba70c8f4714d8fd691cf0cfb01ee927029ee6b
unknown <muhammadyarkhan31@gmail.com>
2026-03-17 23:07:40 +0500

    code updated

--- lecture_04.py
+++ lecture_04.py
@@ -1,10 +1,9 @@
 #Data Preprocessing part 1, This file include code of lecture 04 as well as the code i have added by myself
 #Data cleaning ,outliers detection,finding missing values.
 
-from sklearn.datasets import load_iris
 import pandas as pd
 import seaborn as sns
 
 dataset = sns.load_dataset("titanic")
+print(dataset.shape)
 
-dataset.shape()
