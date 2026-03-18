8067f7ee3bce0caeb86c8d2d9bac35fd9445b6a0
unknown <muhammadyarkhan31@gmail.com>
2026-03-18 23:40:11 +0500

    #Stared feature scalling using standardization as it has outliers

--- lecture_04.py
+++ lecture_04.py
@@ -1,14 +1,16 @@
 #Data Preprocessing part 1, This file include code of lecture 04 as well as the code i have added by myself.
 #Data cleaning ,outliers detection,finding missing values.
 
+
 import pandas as pd
 import seaborn as sns
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 dataset = sns.load_dataset("titanic")
 print(dataset.shape)
 print(dataset.head(10))
-print(dataset.describe()) #this give statistical sunmmary of numerica columns(mean,min max)
+print(dataset.describe()) #this give statistical summary of numerica columns(mean,min max)
 
 #Starting on preprocessing
 print(dataset.isnull().sum())
@@ -29,3 +31,12 @@ print(dataset.isnull().sum())
 label_encoder = LabelEncoder()
 dataset["sex"] = label_encoder.fit_transform(dataset["sex"])
 print(dataset.head(10))
+
+dataset = pd.get_dummies(dataset,columns=["embarked"],dtype=int)
+print(dataset.head(10))
+
+#Stared feature scalling using standardization as it has outliers
+scaler = StandardScaler()
+
+cols = ["age","fare","sibsp", "parch"]
+dataset[cols] = scaler.fit_transform(dataset[cols])
