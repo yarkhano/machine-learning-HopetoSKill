f351e4ade17d8c75951a7202e2d6b2147d169dea
unknown <muhammadyarkhan31@gmail.com>
2026-03-28 22:37:25 +0500

    Evaluating the model

--- lecture_06.py
+++ lecture_06.py
@@ -1,9 +1,12 @@
 #linear regression
+#I have also implemented also some other things not being covered in that lecture
 
 import pandas as pd
+import numpy as np
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error,r2_score
 import matplotlib.pyplot as plt
 
 dataset = fetch_california_housing()
@@ -22,17 +25,29 @@ y_pred = model.predict(X_test)
 
 print(y_pred)
 
-#drawing plots using matplotlib
+
 single_feature = X_test[:,0]
 y_pred = model.predict(X_test)
+#Evaluating the model
+mse = mean_squared_error(y_test,y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test,y_pred)
+print(f'MSE: {mse}')
+print(f'R2: {r2}')
+print(f'RMSE: {rmse}')
 
-plt.scatter(single_feature,y_test,color='blue',label='Actual')
-plt.scatter(single_feature,y_pred,color='red',label='Predicted')
-plt.xlabel('Medinc')
-plt.ylabel("Housing Price")
-plt.legend()
-plt.show()
-# model coefficients m and b
-print("Model Coefficients")
-print(f"slope:{model.coef_[0]}")
-print(f"intercept:{model.intercept_}")
+
+#drawing plots using matplotlib
+# plt.scatter(single_feature,y_test,color='blue',label='Actual')
+# plt.scatter(single_feature,y_pred,color='red',label='Predicted')
+# plt.xlabel('Medinc')
+# plt.ylabel("Housing Price")
+# plt.legend()
+# plt.show()
+#
+#
+#
+# # model coefficients m and b
+# print("Model Coefficients")
+# print(f"slope:{model.coef_[0]}")
+# print(f"intercept:{model.intercept_}")
