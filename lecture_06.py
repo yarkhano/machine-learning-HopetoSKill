b5361c03529c828bac5b51ac29fda80cac2d8249
unknown <muhammadyarkhan31@gmail.com>
2026-03-28 16:06:04 +0500

    imported califonia housing data for linear regression

--- lecture_06.py
+++ lecture_06.py
@@ -1,1 +1,12 @@
 #linear regression
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+dataset = fetch_california_housing.data()
+
+X = dataset.data
+y = dataset.target
+
+X_train,X_test,y_train,y_tset = train_test_split(X,y,test_size=0.2,random_state=42)
