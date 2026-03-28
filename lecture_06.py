a0e4003cb03782530fe2d9b06baffd0a777fcebf
unknown <muhammadyarkhan31@gmail.com>
2026-03-28 22:52:48 +0500

    #did all for linear regression but the dataset I have used is complex so inear regression is unable to capture the patterns

--- lecture_06.py
+++ lecture_06.py
@@ -1,5 +1,7 @@
 #linear regression
-#I have also implemented also some other things not being covered in that lecture
+#I have also implemented  some other things not being covered in that lecture like evaluation,feature scalling
+#the results before feature scalling was average lets scale it and see it then what will happen
+#did all for linear regression but the dataset I have used is complex so inear regression is unable to capture the patterns
 
 import pandas as pd
 import numpy as np
@@ -7,6 +9,7 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 dataset = fetch_california_housing()
@@ -16,18 +19,22 @@ print(df.head())
 X = dataset.data
 y = dataset.target
 
+scaler = StandardScaler()
+
+
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+x_train_scaled = scaler.fit_transform(X_train)
+x_test_scaled = scaler.transform(X_test)
 
 model = LinearRegression()
 
-model.fit(X_train,y_train)
-y_pred = model.predict(X_test)
-
-print(y_pred)
+model.fit(x_train_scaled,y_train)
+y_pred = model.predict(x_test_scaled)
 
 
-single_feature = X_test[:,0]
-y_pred = model.predict(X_test)
+single_feature = x_test_scaled[:,0]
+
 #Evaluating the model
 mse = mean_squared_error(y_test,y_pred)
 rmse = np.sqrt(mse)
@@ -37,17 +44,18 @@ print(f'R2: {r2}')
 print(f'RMSE: {rmse}')
 
 
+
 #drawing plots using matplotlib
-# plt.scatter(single_feature,y_test,color='blue',label='Actual')
-# plt.scatter(single_feature,y_pred,color='red',label='Predicted')
-# plt.xlabel('Medinc')
-# plt.ylabel("Housing Price")
-# plt.legend()
-# plt.show()
-#
-#
-#
-# # model coefficients m and b
-# print("Model Coefficients")
-# print(f"slope:{model.coef_[0]}")
-# print(f"intercept:{model.intercept_}")
+plt.scatter(single_feature,y_test,color='blue',label='Actual')
+plt.scatter(single_feature,y_pred,color='red',label='Predicted')
+plt.xlabel('Medinc')
+plt.ylabel("Housing Price")
+plt.legend()
+plt.show()
+
+
+
+# model coefficients m and b
+print("Model Coefficients")
+print(f"slope:{model.coef_[0]}")
+print(f"intercept:{model.intercept_}")
