699216da55ffd4219ee186b0c9a4f3fd27e9a959
unknown <muhammadyarkhan31@gmail.com>
2026-03-30 22:53:19 +0500

    Working on SNS

--- lecture_07.py
+++ lecture_07.py
@@ -11,3 +11,19 @@ import sklearn
 
 dataset = sns.load_dataset("titanic")
 print(dataset.head())
+print(dataset.info())
+print(dataset.describe())
+
+
+#Here implement matplot part
+
+#Seaborn mostly used here ->Seaborn is builted on the top of Matplolib it provide different colours combinationa
+# and it has dataset-oriented approach to plotting   which is able to work on actual pandas dataframes
+
+color_palate1 = sns.color_palette("Reds")
+sns.palplot(color_palate1)
+plt.show()
+
+#for clor blinds
+sns.palplot(sns.color_palette("colorblind"))
+plt.show()
